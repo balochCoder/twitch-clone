8ed30a4724d7132c404e24076ae32e6f35076a6c
balochCoder <raheelahmed017@gmail.com>
2023-12-13 17:09:19 +0400

    darkMode

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <p className="text-red-500 font-bold">
-      <UserButton/>
+      <UserButton afterSignOutUrl="/"/>
     </p>
   );
 }
